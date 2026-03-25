5e93d6fd17f5492016ec7b8b630f29e39acc3f87
Kaua4570 <150094879+Kaua4570@users.noreply.github.com>
2026-03-25 01:07:46 -0300

    Update service worker cache version and add activation logic

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
-const CACHE = "km-app-v1";
+const CACHE = "km-app-v2";
 
 self.addEventListener("install", e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE).then(cache => {
       return cache.addAll([
         "index.html",
+        "dados.txt",
         "fundo.png",
         "icon-192.png"
       ]);
@@ -12,7 +14,28 @@ self.addEventListener("install", e => {
   );
 });
 
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", e => {
+  if (e.request.url.includes("dados.txt")) {
+    // 🔥 SEM CACHE PRO TXT (sempre atualiza)
+    e.respondWith(fetch(e.request));
+    return;
+  }
+
   e.respondWith(
     caches.match(e.request).then(res => {
       return res || fetch(e.request);
