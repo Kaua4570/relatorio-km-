0da3f4ab1c486a1ed89f4f9b53e20480897467e4
Kaua4570 <150094879+Kaua4570@users.noreply.github.com>
2026-03-25 19:46:33 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,15 @@
-const CACHE = "km-app-v2";
+const CACHE = "km-app-v3";
 
 self.addEventListener("install", e => {
   self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE).then(cache => {
       return cache.addAll([
+        "./",
         "index.html",
         "dados.txt",
-        "fundo.png",
-        "icon-192.png"
+        "icon-192.png",
+        "icon-512.png"
       ]);
     })
   );
@@ -31,9 +32,7 @@ self.addEventListener("activate", e => {
 
 self.addEventListener("fetch", e => {
   if (e.request.url.includes("dados.txt")) {
-    // 🔥 SEM CACHE PRO TXT (sempre atualiza)
-    e.respondWith(fetch(e.request));
-    return;
+    return e.respondWith(fetch(e.request));
   }
 
   e.respondWith(
